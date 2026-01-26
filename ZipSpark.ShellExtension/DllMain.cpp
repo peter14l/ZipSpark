d435c41a05541ee0f2d865af30edcd60c2f2ea44
Peter <peter@zipspark.dev>
2026-01-26 18:43:23 +0530

    Fix: Resolve Linker Errors (g_hInst definition, runtimeobject.lib dependency)

--- ZipSpark.ShellExtension/DllMain.cpp
+++ ZipSpark.ShellExtension/DllMain.cpp
@@ -7,6 +7,8 @@ using namespace Microsoft::WRL;
 // Define the module for the DLL
 CoCreatableClass(ExplorerCommand);
 
+HINSTANCE g_hInst = nullptr;
+
 STDAPI DllGetClassObject(_In_ REFCLSID rclsid, _In_ REFIID riid, _Outptr_ LPVOID* ppv)
 {
     return Module<InProc>::GetModule().GetClassObject(rclsid, riid, ppv);
@@ -21,6 +23,7 @@ STDAPI DllMain(_In_ HINSTANCE hInstance, _In_ DWORD reason, _In_opt_ LPVOID rese
 {
     if (reason == DLL_PROCESS_ATTACH)
     {
+        g_hInst = hInstance;
         DisableThreadLibraryCalls(hInstance);
         Module<InProc>::GetModule().Create();
     }
